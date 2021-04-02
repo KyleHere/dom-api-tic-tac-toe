3a702baea598eebc349f791d3fe7493965d92818
Kyle Tseng <ktseng122@gmail.com>
2021-04-02 16:56:59 -0400

    first commit

---
+++ tic-tac-toe.js
@@ -0,0 +1,33 @@
+let currentPlayerSymbol= "x";
+let squareValues = ["", "", "" , "", "", "", "", "", ""];
+
+window.addEventListener("DOMContentLoaded", () => {
+    console.log(document.getElementById("tic-tac-toe-board"))
+    document
+        .getElementById("tic-tac-toe-board")
+
+        .addEventListener("click", event => {
+            const targetId = event.target.id;
+            console.log(targetId)
+
+            if(!targetId.startsWith("square-")) return;
+
+            const squareIndex = Number.parseInt(targetId[targetId.length-1]);
+
+            if(squareValues[squareIndex] !== "")  return;
+
+            const img = document.createElement("img");
+            img.src = `https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${currentPlayerSymbol}.svg`;
+
+            event.target.appendChild(img);
+
+            squareValues[squareIndex] = currentPlayerSymbol;
+
+            if(currentPlayerSymbol === "x"){
+                currentPlayerSymbol = "o"
+            } else {
+                currentPlayerSymbol = "x";
+            };
+        });
+
+})
